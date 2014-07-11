00f78ab5a01e5613357a15a8910d6c1dd3514ff8
Rob Taylor <roboncode@gmail.com>
2014-07-11 11:43:51 -0600

    Added debounce and throttle

--- build/belt.js
+++ build/belt.js
@@ -15,6 +15,94 @@
     var geom = {};
     var parsers = {};
     var patterns = {};
+    var query;
+    (function() {
+        var fn;
+        function Query(selector, context) {
+            this.init(selector, context);
+        }
+        var queryPrototype = Query.prototype = Object.create(Array.prototype);
+        queryPrototype.version = "0.1.1";
+        queryPrototype.selector = "";
+        queryPrototype.init = function(selector, context) {
+            if (typeof selector === "string") {
+                if (selector.substr(0, 1) === "<" && selector.substr(selector.length - 1, 1) === ">") {
+                    this.parseHTML(selector);
+                } else {
+                    this.parseSelector(selector, context);
+                }
+            } else if (selector instanceof Array) {
+                this.parseArray(selector);
+            } else if (selector instanceof Element) {
+                this.parseElement(selector);
+            }
+        };
+        queryPrototype.parseHTML = function(html) {
+            var container = document.createElement("div");
+            container.innerHTML = html;
+            this.length = 0;
+            this.push(container.firstElementChild);
+        };
+        queryPrototype.parseSelector = function(selector, context) {
+            var i, nodes, len;
+            this.selector = selector;
+            if (context instanceof Element) {
+                this.context = context;
+            } else if (context instanceof Query) {
+                this.context = context[0];
+            } else {
+                this.context = document;
+            }
+            nodes = this.context.querySelectorAll(selector);
+            len = nodes.length;
+            i = 0;
+            this.length = 0;
+            while (i < len) {
+                this.push(nodes[i]);
+                i += 1;
+            }
+        };
+        queryPrototype.parseArray = function(list) {
+            var i = 0, len = list.length;
+            this.length = 0;
+            while (i < len) {
+                if (this[i] instanceof Element) {
+                    this.push(list[i]);
+                }
+                i += 1;
+            }
+        };
+        queryPrototype.parseElement = function(element) {
+            this.length = 0;
+            this.push(element);
+        };
+        queryPrototype.toString = function() {
+            if (this.length) {
+                return this[0].outerHTML;
+            }
+        };
+        queryPrototype.each = function(fn) {
+            var i = 0, len = this.length, result;
+            while (i < len) {
+                result = fn.apply(this[i], [ i, this[i] ]);
+                if (result === false) {
+                    break;
+                }
+                i += 1;
+            }
+            return this;
+        };
+        query = function(selector, context) {
+            for (var n in query.fn) {
+                if (query.fn.hasOwnProperty(n)) {
+                    queryPrototype[n] = query.fn[n];
+                    delete query.fn[n];
+                }
+            }
+            return new Query(selector, context);
+        };
+        query.fn = {};
+    })();
     var timers = {};
     var validators = {};
     var xml = {};
@@ -1759,6 +1847,22 @@
         });
         return "[" + value.join(", ") + "]";
     };
+    var debounce = function(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function() {
+                timeout = null;
+                if (!immediate) {
+                    func.apply(context, args);
+                }
+            }, wait);
+            if (immediate && !timeout) {
+                func.apply(context, args);
+            }
+        };
+    };
     var forEach = function(obj, iterator, context) {
         var key;
         if (obj) {
@@ -1786,6 +1890,24 @@
         var f = typeof fn === "function";
         var s = f && (fn.name && [ "", fn.name ] || fn.toString().match(/function ([^\(]+)/));
         return !f && "not a function" || (s && s[1] || "anonymous");
+    };
+    var throttle = function(func, threshhold, scope) {
+        threshhold = threshhold || 250;
+        var last, deferTimer;
+        return function() {
+            var context = scope || this;
+            var now = +new Date(), args = arguments;
+            if (last && now < last + threshhold) {
+                clearTimeout(deferTimer);
+                deferTimer = setTimeout(function() {
+                    last = now;
+                    func.apply(context, args);
+                }, threshhold);
+            } else {
+                last = now;
+                func.apply(context, args);
+            }
+        };
     };
     geom.Rect = function(x, y, width, height) {
         this.x = x || 0;
@@ -3286,6 +3408,420 @@
             };
         }
     })();
+    query.fn.bind = query.fn.on = function(event, handler) {
+        this.each(function(index, el) {
+            if (el.attachEvent) {
+                el["e" + event + handler] = handler;
+                el[event + handler] = function() {
+                    el["e" + event + handler](window.event);
+                };
+                el.attachEvent("on" + event, el[event + handler]);
+            } else {
+                el.addEventListener(event, handler, false);
+            }
+            if (!el.eventHolder) {
+                el.eventHolder = [];
+            }
+            el.eventHolder[el.eventHolder.length] = [ event, handler ];
+        });
+        return this;
+    };
+    query.fn.change = function(handler) {
+        if (validators.isDefined(handler)) {
+            this.on("change", handler);
+        } else {
+            this.trigger("change");
+        }
+        return this;
+    };
+    query.fn.click = function(handler) {
+        if (validators.isDefined(handler)) {
+            this.bind("click", handler);
+        } else {
+            this.trigger("click");
+        }
+        return this;
+    };
+    query.fn.trigger = function(eventName, data) {
+        var event;
+        if (document.createEvent) {
+            event = document.createEvent("HTMLEvents");
+            event.initEvent(eventName, true, true);
+        } else {
+            event = document.createEventObject();
+            event.eventType = eventName;
+        }
+        event.eventName = eventName;
+        event.data = data;
+        this.each(function(index, el) {
+            if (document.createEvent) {
+                el.dispatchEvent(event);
+            } else {
+                el.fireEvent("on" + event.eventType, event);
+            }
+        });
+        return this;
+    };
+    query.fn.unbind = query.fn.off = function(event, handler) {
+        if (arguments.length === 1) {
+            this.unbindAll(event);
+        } else {
+            this.each(function(index, el) {
+                if (el.detachEvent) {
+                    el.detachEvent("on" + event, el[event + handler]);
+                    el[event + handler] = null;
+                } else {
+                    el.removeEventListener(event, handler, false);
+                }
+            });
+        }
+        return this;
+    };
+    query.fn.unbindAll = function(event) {
+        var scope = this;
+        this.each(function(index, el) {
+            if (el.eventHolder) {
+                var removed = 0, handler;
+                for (var i = 0; i < el.eventHolder.length; i++) {
+                    if (el.eventHolder[i][0] === event) {
+                        handler = el.eventHolder[i][1];
+                        scope.off(el, event, handler);
+                        if (el.detachEvent) {
+                            el.detachEvent("on" + event, el[event + handler]);
+                            el[event + handler] = null;
+                        } else {
+                            el.removeEventListener(event, handler, false);
+                        }
+                        el.eventHolder.splice(i, 1);
+                        removed += 1;
+                        i -= 1;
+                    }
+                }
+            }
+        });
+        return this;
+    };
+    query.fn.height = function() {
+        return this.css("height");
+    };
+    query.fn.innerHeight = function() {
+        return this.css("innerHeight");
+    };
+    query.fn.innerWidth = function() {
+        return this.css("innerWidth");
+    };
+    query.fn.offset = function() {
+        if (this.length) {
+            return this[0].getBoundingClientRect();
+        }
+    };
+    query.fn.outerHeight = function() {
+        return this.css("outerHeight");
+    };
+    query.fn.outerWidth = function() {
+        return this.css("outerWidth");
+    };
+    query.fn.width = function() {
+        return this.css("width");
+    };
+    query.fn.removeAttr = function(prop) {
+        this.each(function(index, el) {
+            el.removeAttribute(prop);
+        });
+        return this;
+    };
+    query.fn.attr = function(prop, value) {
+        if (arguments.length === 2) {
+            if (typeof value === "function") {
+                this.each(function(index, el) {
+                    var result = value.apply(el, [ index, prop ]);
+                    this.setAttribute(prop, result);
+                });
+            } else {
+                this.each(function(index, el) {
+                    el.setAttribute(prop, value);
+                });
+            }
+            return this;
+        }
+        if (typeof prop === "object") {
+            this.each(function(index, el) {
+                for (var n in prop) {
+                    if (prop.hasOwnProperty(n)) {
+                        el.setAttribute(n, prop[n]);
+                    }
+                }
+            });
+            return this;
+        }
+        if (this.length) {
+            return this[0].getAttribute(prop);
+        }
+    };
+    query.fn.data = function(prop, value) {
+        return this.attr("data-" + prop, value);
+    };
+    query.fn.addClass = function(className) {
+        var scope = this;
+        this.each(function(index, el) {
+            if (!scope.hasClass(el, className)) {
+                el.className += " " + className;
+            }
+        });
+        return this;
+    };
+    query.fn.hasClass = function(className) {
+        var el = this[0];
+        if (el.classList) {
+            return el.classList.contains(className);
+        } else {
+            return new RegExp("(^| )" + className + "( |$)", "gi").test(el.className);
+        }
+        return false;
+    };
+    query.fn.removeClass = function(className) {
+        var scope = this;
+        this.each(function(index, el) {
+            if (isDefined(className)) {
+                var newClass = " " + el.className.replace(/[\t\r\n]/g, " ") + " ";
+                if (scope.hasClass(el, className)) {
+                    while (newClass.indexOf(" " + className + " ") >= 0) {
+                        newClass = newClass.replace(" " + className + " ", " ");
+                    }
+                    el.className = newClass.replace(/^\s+|\s+$/g, "");
+                }
+            } else {
+                el.className = "";
+            }
+        });
+        return this;
+    };
+    query.fn.css = function(prop, value) {
+        var el, returnValue;
+        if (this.length) {
+            el = this[0];
+            if (arguments.length > 1) {
+                this.each(function(index, el) {
+                    el.style[prop] = value;
+                });
+            }
+            if (prop instanceof Array) {
+                var i = 0, len = prop.length;
+                returnValue = {};
+                if (el.currentStyle) {
+                    while (i < len) {
+                        returnValue[prop[i]] = el.currentStyle[prop[i]];
+                        i += 1;
+                    }
+                } else if (window.getComputedStyle) {
+                    while (i < len) {
+                        returnValue[prop[i]] = document.defaultView.getComputedStyle(el, null).getPropertyValue(prop[i]);
+                        i += 1;
+                    }
+                }
+            } else {
+                if (el.currentStyle) {
+                    returnValue = el.currentStyle[prop];
+                } else if (window.getComputedStyle) {
+                    returnValue = document.defaultView.getComputedStyle(el, null).getPropertyValue(prop);
+                }
+            }
+            return returnValue;
+        }
+        return null;
+    };
+    query.fn.prop = function(name, value) {
+        if (this.length) {
+            if (arguments.length > 2) {
+                this[0][name] = value;
+            } else {
+                return this[0][name];
+            }
+        }
+    };
+    query.fn.is = function(name) {
+        name = name.split(":").join("");
+        return this.prop(name);
+    };
+    query.fn.after = function(content, elements) {};
+    query.fn.append = function(element) {
+        if (typeof element === "string") {
+            element = query(element);
+        }
+        if (element instanceof Array) {
+            if (element.length) {
+                element = element[0];
+            }
+        }
+        if (element instanceof Element || element instanceof Node) {
+            this.each(function(index, el) {
+                el.appendChild(element);
+            });
+        }
+    };
+    query.fn.before = function(content, elements) {};
+    query.fn.empty = function() {
+        this.each(function(index, el) {
+            el.innerHTML = null;
+        });
+    };
+    query.fn.html = function(val) {
+        if (this.length) {
+            var el = this[0];
+            if (arguments.length > 0) {
+                this.each(function(index, el) {
+                    el.innerHTML = val;
+                });
+            }
+            return el.innerHTML;
+        }
+    };
+    query.fn.prepend = function(element) {
+        if (typeof element === "string") {
+            element = query(element);
+        }
+        if (element instanceof Array) {
+            if (element.length) {
+                element = element[0];
+            }
+        }
+        if (element instanceof Element || element instanceof Node) {
+            this.each(function(index, el) {
+                if (el.childNodes.length) {
+                    el.insertBefore(element, el.childNodes[0]);
+                } else {
+                    el.appendChild(element);
+                }
+            });
+        }
+    };
+    query.fn.remove = function() {
+        this.each(function(index, el) {
+            if (el.parentElement) {
+                el.parentElement.removeChild(el);
+            }
+        });
+    };
+    query.fn.text = function(val) {
+        if (this.length) {
+            var el = this[0];
+            if (arguments.length > 0) {
+                this.each(function(index, el) {
+                    el.innerText = val;
+                });
+            }
+            return el.innerText;
+        }
+    };
+    query.fn.isChecked = function() {
+        if (this.length) {
+            return this[0].checked;
+        }
+        return false;
+    };
+    query.fn.isVisible = function() {
+        var el;
+        if (this.length) {
+            el = this[0];
+            if (el.parentNode.nodeType === 9) {
+                return true;
+            }
+            if (el.offsetWidth === 0 || el.offsetHeight === 0) {
+                return false;
+            }
+            if (this.css(el, "opacity") === 0 || this.css(el, "display") === "none" || this.css("visibility") === "hidden") {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    };
+    query.fn.val = function(value) {
+        var el, result, i, len, options;
+        if (this.length) {
+            el = this[0];
+            if (arguments.length) {
+                el.value = value;
+            } else {
+                if (el.nodeName === "SELECT" && el.multiple) {
+                    result = [];
+                    i = 0;
+                    options = el.options;
+                    len = options.length;
+                    while (i < len) {
+                        if (options) {
+                            result.push(options[i].value || options[0].text);
+                        }
+                    }
+                    return result.length === 0 ? null : result;
+                }
+                return el.value;
+            }
+        }
+    };
+    query.fn.children = function() {};
+    query.fn.find = function(selector) {
+        if (this.length) {
+            return query(selector, this[0]);
+        }
+        return query();
+    };
+    query.fn.first = function(returnElement) {
+        if (this.length) {
+            if (returnElement) {
+                return this[0];
+            }
+            return query(this[0]);
+        }
+        if (returnElement) {
+            return null;
+        }
+        return query();
+    };
+    query.fn.get = function(index) {
+        if (isDefined(index)) {
+            if (Math.abs(index) < this.length) {
+                if (index < 0) {
+                    return this[this.length + index - 1];
+                }
+                return this[index];
+            }
+            return this;
+        }
+        return this.splice(0);
+    };
+    query.fn.last = function(returnElement) {
+        if (this.length) {
+            if (returnElement) {
+                return this[this.length - 1];
+            }
+            return query(this[this.length - 1]);
+        }
+        if (returnElement) {
+            return null;
+        }
+        return query();
+    };
+    query.fn.next = function() {};
+    query.fn.not = function(selector) {
+        if (this.length) {
+            return query(":not(" + selector + ")", this[0]);
+        }
+        return query();
+    };
+    query.fn.parent = function(selector) {
+        if (this.length) {
+            var parent = this[0].parentNode;
+            if (parent && parent.nodeType !== 11) {
+                if (selector) {
+                    return query(parent).find(selector);
+                }
+                return query(parent);
+            }
+        }
+        return query();
+    };
+    query.fn.prev = function() {};
     timers.Timer = function(delay, repeat, limit) {
         var count, t, scope = this;
         function check() {
@@ -3335,6 +3871,9 @@
     };
     validators.isDate = function(val) {
         return val instanceof Date;
+    };
+    validators.isUndefined = function(val) {
+        return typeof val !== "undefined";
     };
     validators.isEmail = function(value) {
         var regExp = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9])+$/;
@@ -3476,11 +4015,14 @@
     exports["geom"] = geom;
     exports["parsers"] = parsers;
     exports["patterns"] = patterns;
+    exports["query"] = query;
     exports["timers"] = timers;
     exports["validators"] = validators;
     exports["xml"] = xml;
+    exports["debounce"] = debounce;
     exports["forEach"] = forEach;
     exports["getName"] = getName;
+    exports["throttle"] = throttle;
 })({}, function() {
     return this;
 }());
