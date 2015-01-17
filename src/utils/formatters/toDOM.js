9b79417dedef86b32d472c386f23d0c75327d802
Rob Taylor <roboncode@gmail.com>
2015-01-16 19:55:15 -0700

    update toDOM using firstElementChild

--- src/utils/formatters/toDOM.js
+++ src/utils/formatters/toDOM.js
@@ -4,7 +4,7 @@ define('toDOM', function () {
     var htmlToDOM = function (htmlStr) {
         var container = document.createElement('div');
         container.innerHTML = htmlStr;
-        return container.firstChild;
+        return container.firstElementChild;
     };
 
     return htmlToDOM;
