48db35c4404340d5d708dfa7a5bfe45fe5c1c558
wesjones <cybus10@gmail.com>
2015-02-11 14:37:43 -0700

    bumped to version 0.5.8 css.js for query added ability to pass objects for css.

--- src/utils/query/modify/css.js
+++ src/utils/query/modify/css.js
@@ -5,7 +5,7 @@
  */
 internal('query.css', ['query'], function (query) {
     query.fn.css = function (prop, value) {
-        var el, returnValue;
+        var el, returnValue, i, len;
         if (this.length) {
             el = this[0];
 
@@ -13,10 +13,17 @@ internal('query.css', ['query'], function (query) {
                 this.each(function (index, el) {
                     el.style[prop] = value;
                 });
+            } else if (arguments.length === 1 && typeof prop === "object") {
+                for(i in prop) {
+                    if (prop.hasOwnProperty(i)) {
+                        el.style[i] = prop[i];
+                    }
+                }
             }
 
             if (prop instanceof Array) {
-                var i = 0, len = prop.length;
+                i = 0;
+                len = prop.length;
                 returnValue = {};
                 if (el.currentStyle) {
                     while (i < len) {
@@ -27,6 +34,21 @@ internal('query.css', ['query'], function (query) {
                     while (i < len) {
                         returnValue[prop[i]] = document.defaultView.getComputedStyle(el, null).getPropertyValue(prop[i]);
                         i += 1;
+                    }
+                }
+            } else if (typeof prop === "object") {
+                returnValue = {};
+                if (el.currentStyle) {
+                    for (i in prop) {
+                        if (prop.hasOwnProperty(i)) {
+                            returnValue[prop[i]] = el.currentStyle[prop[i]];
+                        }
+                    }
+                } else if (window.getComputedStyle) {
+                    for (i in prop) {
+                        if (prop.hasOwnProperty(i)) {
+                            returnValue[prop[i]] = document.defaultView.getComputedStyle(el, null).getPropertyValue(prop[i]);
+                        }
                     }
                 }
             } else {
