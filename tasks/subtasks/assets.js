a4416ba12ea0e51108e40e805011a5e532dcf35b
wesjones <cybus10@gmail.com>
2015-03-28 08:59:26 -0600

    bumped to version 0.5.47 remove hard coded build/css that was created by assets.

--- tasks/subtasks/assets.js
+++ tasks/subtasks/assets.js
@@ -1,20 +1,14 @@
 exports.run = function (grunt, wrap, filename, data) {
     //find all assets directories.
     //put them in the styles path directory under assets.
-    if (data.styles) {
-        var copy = {
-            files: [
-                {
-                    expand: true,
-                    src: ['src/**/*.svg','src/**/*.png','src/**/*.gif','src/**/*.jpg'],
-                    dest: 'build/css/',
-                    filter: 'isFile',
-                    flatten: true
-                }
-            ]
-        };
+    var copy = {files:[]}, config;
 
-        var config = grunt.config.get('copy') || {};
+    if (data.assets) {
+        copy.files = copy.files.concat(data.assets.files);
+    }
+    
+    if (copy.files.length) {
+        config = grunt.config.get('copy') || {};
         config['hummingbird_copy'] = copy;
         grunt.config.set('copy', config);
         grunt.task.run('copy:hummingbird_copy');
