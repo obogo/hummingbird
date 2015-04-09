c817acb6eeea9a415479a5a9fdce67fd415a580b
Rob Taylor <roboncode@gmail.com>
2015-04-09 13:12:22 -0600

    added apply() to utils

---
+++ src/utils/data/apply.js
@@ -0,0 +1,22 @@
+define('apply', function () {
+    return function (func, scope, args) {
+        args = args || [];
+        switch (args.length) {
+            case 0:
+                return func();
+            case 1:
+                return func.call(scope, args[0]);
+            case 2:
+                return func.call(scope, args[0], args[1]);
+            case 3:
+                return func.call(scope, args[0], args[1], args[2]);
+            case 4:
+                return func.call(scope, args[0], args[1], args[2], args[3]);
+            case 5:
+                return func.call(scope, args[0], args[1], args[2], args[3], args[4]);
+            case 6:
+                return func.call(scope, args[0], args[1], args[2], args[3], args[4], args[5]);
+        }
+        return func.apply(scope, args);
+    };
+})
