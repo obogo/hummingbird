29b24ecc36d803c9eca7b5dc82249f2072134ac9
wesjones <cybus10@gmail.com>
2015-09-29 10:14:04 -0600

    for some reason unless it is a define, it cannot be put on the external api.

--- src/utils/geom/getDistanceToRect.js
+++ src/utils/geom/getDistanceToRect.js
@@ -1,4 +1,4 @@
-internal('getDistanceToRect', [], function() {
+define('getDistanceToRect', [], function() {
     return function(rect, pt) {
         var cx = Math.max(Math.min(pt.x, rect.x+rect.width ), rect.x);
         var cy = Math.max(Math.min(pt.y, rect.y+rect.height), rect.y);
