def50bb254ad06b771e5fed2b2b7c500fcd39c65
wesjones <cybus10@gmail.com>
2015-11-09 14:09:56 -0700

    dispatcher on method has dispatcher flag to set as recognizable

--- src/utils/async/dispatcher.js
+++ src/utils/async/dispatcher.js
@@ -80,6 +80,7 @@ define('dispatcher', ['apply', 'isFunction'], function (apply, isFunction) {
                 };
             }
         }
+        on.dispatcher = true;
 
         /**
          * ###once###
