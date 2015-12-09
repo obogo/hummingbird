79b5ea94611a68f19ba210bdea3bf050ecf58419
wesjones <cybus10@gmail.com>
2015-12-08 17:16:45 -0700

    dynamic worker not supported in IE10. Removed attempted support.

--- src/utils/async/worker.js
+++ src/utils/async/worker.js
@@ -1,5 +1,4 @@
 define('worker', ['apply'], function (apply) {
-    var isIE10 = navigator.appVersion.indexOf("MSIE 10") !== -1;
     function createWorker(fn) {
         // URL.createObjectURL
         window.URL = window.URL || window.webkitURL;
@@ -15,28 +14,6 @@ define('worker', ['apply'], function (apply) {
             blob = new BlobBuilder();
             blob.append(response);
             blob = blob.getBlob();
-        }
-        // if IE10. It throws a security exception. So fake the worker.
-        if (isIE10) {
-            return {
-                onmessage: null,
-                postMessage: function (txt) {
-                    var e = {data: txt};
-                    var self = this;
-                    //fn = fn.split("postMessage(").join("this.postMessage(");
-                    fn = fn.replace(/^function\s[\w\d]+\(.*?\)\s+\{/, '');
-                    fn = fn.substr(0, fn.length - 2);
-                    var f = apply(new Function('with(this) {' + fn + '}'), {
-                        e: e, postMessage: function (result) {
-                            setTimeout(function () {
-                                self.onmessage({data: result});
-                            });
-                        }
-                    }, [e]);
-                },
-                terminate: function () {
-                }
-            };
         }
         return new Worker(URL.createObjectURL(blob));
     }
