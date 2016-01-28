5b280e96959cd0635c97e3286589f785c5ef4696
wesjones <cybus10@gmail.com>
2016-01-27 17:15:39 -0700

    revert fix for toTime filter

--- src/hb/filters/toTime.js
+++ src/hb/filters/toTime.js
@@ -1,7 +1,9 @@
 /* global internal */
 //! pattern /(\|toTime|\(\'toTime\'\))/
 internal('hbf.toTime', ['hb.filter', 'toTime'], function (filter, toTime) {
-    filter('toTime', toTime);
+    filter('toTime', function() {
+        return toTime;
+    });
 });
 
 
