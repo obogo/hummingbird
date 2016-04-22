f3682a68369969b54050f6aa5bad3bb5d49cdfb8
wesjones <cybus10@gmail.com>
2016-04-22 11:32:47 -0600

    fix template model so that multiple load of the same url will not invoke multiple requests but wait for the first one to complete and then fire all callbacks.

--- src/hb/utils/template.js
+++ src/hb/utils/template.js
@@ -1,23 +1,37 @@
 define('hb.template', ['http', 'hb.debug', 'each'], function(http, debug, each) {
+    var loading = {};
+
+    function onLoad($app, url) {
+        while (loading[url].length) {
+            loading[url].shift()($app.template(url));
+        }
+        delete loading[url];
+    }
+
     function loadTemplate($app, url, callback) {
+        var isLoading = !!loading[url];
+        loading[url] = loading[url] || [];
+        loading[url].push(callback);
         if (!$app.template(url)) {
-            var u = ($app.template('templatesBaseUrl') || '') + url;
-            debug.info('load template', u);
-            http.get({
-                url: u,
-                //async: false,// this MUST be synchronous.
-                success: function (r) {
-                    $app.template(url, r.data);
-                    callback($app.template(url));
-                },
-                error: function () {
-                    $app.template(url, '<div class="e404">OOPS! "' + u + '" - 404 Not Found!</div>');
-                    callback($app.template(url));
-                }
-            });
+            if (!isLoading) {
+                var u = ($app.template('templatesBaseUrl') || '') + url;
+                debug.info('load template', u);
+                http.get({
+                    url: u,
+                    //async: false,// this MUST be synchronous.
+                    success: function (r) {
+                        $app.template(url, r.data);
+                        onLoad($app, url);
+                    },
+                    error: function () {
+                        $app.template(url, '<div class="e404">OOPS! "' + u + '" - 404 Not Found!</div>');
+                        onLoad($app, url);
+                    }
+                });
+            }
             return;
         }
-        callback($app.template(url));
+        onLoad($app, url);
     }
 
     function loadTemplateItem(item, index, list, $app, next) {
