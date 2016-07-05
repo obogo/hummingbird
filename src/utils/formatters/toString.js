7b827c5d3ee2a70e70a498ce1720330affaba893
Robert Taylor <roboncode@gmail.com>
2016-07-05 07:29:37 -0600

    toString fix

--- src/utils/formatters/toString.js
+++ src/utils/formatters/toString.js
@@ -1,9 +1,11 @@
 define('toString', function () {
     var toString = function () {
         var value = [];
-        forEach(this, function (e) {
-            value.push('' + e);
-        });
+        for (var e in this) {
+            if (this.hasOwnProperty(e)) {
+                value.push('' + e);
+            }
+        }
         return '[' + value.join(', ') + ']';
     };
 
