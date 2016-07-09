fe48aabdd7ab59f89e053611b727d4456615ef4a
Robert Taylor <roboncode@gmail.com>
2016-07-08 20:53:17 -0600

    isArguments is defining toString() internally

--- src/utils/validators/isArguments.js
+++ src/utils/validators/isArguments.js
@@ -1,14 +1,24 @@
-define('isArguments', ['toString'], function (toString) {
+define('isArguments', function () {
+    var toString = function () {
+        var value = [];
+        for (var e in this) {
+            if (this.hasOwnProperty(e)) {
+                value.push('' + e);
+            }
+        }
+        return '[' + value.join(', ') + ']';
+    };
+
     var isArguments = function (value) {
         var str = String(value);
         var isArguments = str === '[object Arguments]';
         if (!isArguments) {
             isArguments = str !== '[object Array]' &&
-            value !== null &&
-            typeof value === 'object' &&
-            typeof value.length === 'number' &&
-            value.length >= 0 &&
-            (!value.callee || toString.call(value.callee) === '[object Function]');
+                value !== null &&
+                typeof value === 'object' &&
+                typeof value.length === 'number' &&
+                value.length >= 0 &&
+                (!value.callee || toString.call(value.callee) === '[object Function]');
         }
         return isArguments;
     };
