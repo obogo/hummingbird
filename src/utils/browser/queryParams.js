e2754c9b8506af48a78564dd679bb41531f0545b
Robert Taylor <roboncode@gmail.com>
2016-07-11 07:46:55 -0600

    fix queryParams bug with param argument

--- src/utils/browser/queryParams.js
+++ src/utils/browser/queryParams.js
@@ -1,16 +1,16 @@
 define('queryParams', function () {
-    function queryParams(queryString) {
+    function queryParams(param) {
         var regex = /([^&=]+)=?([^&]*)/g;
         var match, store = {};
 
-        var haystack = queryString || window.location.search || window.location.hash;
+        var haystack = window.location.search || window.location.hash;
         haystack = haystack.substring(haystack.indexOf('?') + 1, haystack.length);
 
         while ((match = regex.exec(haystack))) {
             store[decodeURIComponent(match[1])] = decodeURIComponent(match[2]);
         }
 
-        return store;
+        return store[param];
     }
 
     return queryParams;
