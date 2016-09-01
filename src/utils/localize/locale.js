70250e4e57a98a910a7a7a1b0c093a4a14290930
wesjones <cybus10@gmail.com>
2016-09-01 12:48:02 -0600

    update locale to support multple calls to load.

--- src/utils/localize/locale.js
+++ src/utils/localize/locale.js
@@ -1,5 +1,7 @@
 define('locale', ['http', 'defer', 'extend', 'resolve', 'format'], function (http, defer, extend, resolve, format) {
     var r;
+    var lang = '';
+    var loading = false;
     var defaults = {
         monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
@@ -10,29 +12,42 @@ define('locale', ['http', 'defer', 'extend', 'resolve', 'format'], function (htt
         }
         return r.get(key);
     };
-    locale.$load = function (lang) {
-        var d = defer();
-        lang = lang.toLowerCase();
-        locale("language.label", lang);
-        http.get({
-            url: 'locale/' + lang + '.json',
-            success: function (response) {
-                extend(locale, response.data);
-                Date.setLocalization(locale.monthNames, locale.dayNames);
-                d.resolve(locale);
-            },
-            error: function (e) {
-                d.reject(e);
-                locale.$load('en-us');
-            }
-        });
-        return d.promise;
+    locale.$load = function (language) {
+        language = language || window.navigator.language || window.navigator.userLanguage;
+        if (lang !== language) {
+            lang = language;
+            loading = null;
+        }
+        if (!loading) {
+            var d = loading = defer();
+            lang = lang.toLowerCase();
+            locale("language.label", lang);
+            http.get({
+                url: 'locale/' + lang + '.json',
+                success: function (response) {
+                    extend(locale, response.data);
+                    Date.setLocalization(locale.monthNames, locale.dayNames);
+                    if (loading === d) {
+                        loading = null;
+                    }
+                    d.resolve(locale);
+                },
+                error: function (e) {
+                    if (loading === d) {
+                        loading = null;
+                    }
+                    d.reject(e);
+                    locale.$load('en-us');
+                }
+            });
+        }
+        return loading.promise;
     };
 
     r = resolve(locale);
     extend(locale, defaults);
     Date.setLocalization(locale.monthNames, locale.dayNames);
-    locale.$load(window.navigator.language || window.navigator.userLanguage);
+    locale.$load();
 
     return locale;
 });
