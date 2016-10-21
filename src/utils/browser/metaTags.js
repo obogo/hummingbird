3b1c2737392fdd3ec6c7d1bef7d4a4fc82a3b3df
wesjones <cybus10@gmail.com>
2016-10-21 08:19:43 -0600

    add set to metaTags

--- src/utils/browser/metaTags.js
+++ src/utils/browser/metaTags.js
@@ -1,12 +1,23 @@
 define('metaTags', function() {
+    var d = document;
     var data = {
+        $set: function(key, value) {
+            var node = d.querySelector("meta[name='" + key + "']");
+            if (!node) {
+                node = d.createElement('meta');
+                d.head.appendChild(node);
+            }
+            node.setAttribute('name', key);
+            node.setAttribute('content', value);
+            data[key] = value;
+        },
         $refresh: function() {
             data = refresh();
             return this;
         }
     };
     function refresh() {
-        var m = document.querySelectorAll("meta");
+        var m = d.querySelectorAll("meta");
         for (var i = 0; i < m.length; i += 1) {
             data[m[i].getAttribute("name")] = m[i].getAttribute("content")
         }
