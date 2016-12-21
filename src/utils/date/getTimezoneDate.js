24cccac31d6dfcd5e93640859cbf103bb03200be
wesjones <cybus10@gmail.com>
2016-12-21 13:11:17 -0700

    add timezone date

--- src/utils/date/getTimezoneDate.js
+++ src/utils/date/getTimezoneDate.js
@@ -1,7 +1,7 @@
 define('getTimezoneDate', function() {
     return function (timezoneOffset) {
         if (isNaN(timezoneOffset)) {
-            throw new Error("Expected " + + " to be a Number instead got type " + typeof timezoneOffset);
+            return '';
         }
         var myTz = new Date().getTimezoneOffset();
         return new Date(Date.now() - ((timezoneOffset - myTz) * 60 * 1000));
