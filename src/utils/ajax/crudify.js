ce20879aa910249d1c3ae9afec07fde0e3d0fa7b
Robert Taylor <roboncode@gmail.com>
2017-03-01 13:33:52 -0700

    Added crudify and resources to ajax dir, fix to cookie

---
+++ src/utils/ajax/crudify.js
@@ -0,0 +1,325 @@
+/* global define */
+define('services.crudify', ['services.resource', 'defer', 'http', 'inflection', 'extend'], function (resource, defer, http, inflection, extend) {
+
+    var $methods = {};
+
+    var onSuccess, onError;
+
+    var capitalize = function (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    };
+
+    var trimSlashes = function (str) {
+        return str.replace(/^\/?(.*?)\/?$/, '$1');
+    };
+
+    var singularizeCapitalize = function (str) {
+        str = inflection.singularize(str) || str;
+        str = capitalize(str);
+        return str;
+    };
+
+    var requireParam = function (key, value) {
+        if (typeof value === 'undefined') {
+            throw new Error('Expected param ' + key + ' to be defined: ' + JSON.stringify(value));
+        }
+    };
+
+    var requireId = function (value) {
+        requireParam('id', value);
+        var type = typeof value;
+        if (!(type === 'number' || type === 'string')) {
+            throw new Error('Expected param "id" to be "number" or "string": ' + JSON.stringify(value));
+        }
+    };
+
+    var requireData = function (value) {
+        requireParam('data', value);
+        var type = typeof value;
+        if (type !== 'object') {
+            throw new Error('Expected param "data" to be "object": ' + JSON.stringify(value));
+        }
+    };
+
+    $methods.all = function (name, options) {
+        return function (params) {
+            params = extend({}, params, http.defaults.params);
+
+            var deferred = defer();
+            var payload = {};
+            payload.credentials = !!options.withCredentials; //[string replacement]
+            payload.url = resource({baseUrl: options.baseUrl}).resource(name).params(params).toUrl();
+            payload.success = deferred.resolve;
+            payload.error = deferred.reject;
+            http.get(payload);
+            var promise = deferred.promise;
+
+            promise.success(onSuccess);
+            promise.error(onError);
+
+            return promise;
+        };
+    };
+
+    $methods.create = function (name, options) {
+        return function (data, params) {
+
+            requireData(data);
+
+            params = extend({}, params, http.defaults.params);
+
+            var deferred = defer();
+            var payload = {};
+            payload.credentials = !!options.withCredentials; //[string replacement]
+            payload.url = resource({baseUrl: options.baseUrl}).resource(name).params(params).toUrl();
+            payload.data = data;
+            payload.success = deferred.resolve;
+            payload.error = deferred.reject;
+            http.post(payload);
+
+            var promise = deferred.promise;
+
+            promise.success(onSuccess);
+            promise.error(onError);
+
+            return promise;
+        };
+    };
+
+    $methods.get = function (name, options) {
+
+        return function (id, params) {
+
+            requireId(id);
+
+            params = extend({}, params, http.defaults.params);
+
+            var deferred = defer();
+            var payload = {};
+            payload.credentials = !!options.withCredentials; //[string replacement]
+            payload.url = resource({baseUrl: options.baseUrl}).resource(options.uri || name, id).params(params).toUrl();
+            payload.success = deferred.resolve;
+            payload.error = deferred.reject;
+            http.get(payload);
+
+            var promise = deferred.promise;
+
+            promise.success(onSuccess);
+            promise.error(onError);
+
+            return promise;
+        };
+    };
+
+    $methods.update = function (name, options) {
+        var path = '';
+        if(options.useGETPOSTonly === false) {
+            path = 'update';
+        }
+
+        return function (id, data, params) {
+            requireId(id);
+            requireData(data);
+
+            params = extend({}, params, http.defaults.params);
+
+            var deferred = defer();
+            var payload = {};
+            payload.credentials = !!options.withCredentials;
+            payload.url = resource({baseUrl: options.baseUrl}).resource(name, id).resource(path).params(params).toUrl();
+            payload.data = data;
+            payload.success = deferred.resolve;
+            payload.error = deferred.reject;
+            if(options.useGETPOSTonly === false) {
+                http.post(payload);
+            } else {
+                http.put(payload);
+            }
+            var promise = deferred.promise;
+
+            promise.success(onSuccess);
+            promise.error(onError);
+
+            return promise;
+        };
+    };
+
+    $methods.delete = function (name, options) {
+        var path = '';
+        if(options.useGETPOSTonly === false) {
+            path = 'delete';
+        }
+
+        return function (id, params) {
+
+            requireId(id);
+
+            params = extend({}, params, http.defaults.params);
+
+            var deferred = defer();
+            var payload = {};
+            payload.credentials = !!options.withCredentials; //[string replacement]
+            payload.url = resource({baseUrl: options.baseUrl}).resource(name, id).resource(path).params(params).toUrl();
+            payload.success = deferred.resolve;
+            payload.error = deferred.reject;
+
+            if(options.useGETPOSTonly === false) {
+                http.post(payload);
+            } else {
+                http.delete(payload);
+            }
+
+            var promise = deferred.promise;
+
+            promise.success(onSuccess);
+            promise.error(onError);
+
+            return promise;
+        };
+    };
+
+    $methods.count = function (name, options) {
+        return function (params) {
+            params = extend({}, params, http.defaults.params);
+
+            var deferred = defer();
+            var payload = {};
+            payload.credentials = !!options.withCredentials; //[string replacement]
+            payload.url = resource({baseUrl: options.baseUrl}).resource(name).resource('count').params(params).toUrl();
+            payload.success = deferred.resolve;
+            payload.error = deferred.reject;
+            http.get(payload);
+
+            var promise = deferred.promise;
+
+            promise.success(onSuccess);
+            promise.error(onError);
+
+            return promise;
+        };
+    };
+
+    $methods.exists = function (name, options) {
+        return function (params) {
+            params = extend({}, params, http.defaults.params);
+
+            var deferred = defer();
+            var payload = {};
+            payload.credentials = !!options.withCredentials; //[string replacement]
+            payload.url = resource({baseUrl: options.baseUrl}).resource(name).resource('exists').params(params).toUrl();
+            payload.success = deferred.resolve;
+            payload.error = deferred.reject;
+            http.get(payload);
+
+            var promise = deferred.promise;
+
+            promise.success(onSuccess);
+            promise.error(onError);
+
+            return promise;
+        };
+    };
+
+    return function (target, resource, options) {
+
+        options = extend({}, resource, options);
+
+        onSuccess = function (response) {
+            target.fire('success', response);
+        };
+
+        onError = function (response) {
+            target.fire('error', response);
+        };
+
+        var methods = resource.methods;
+        if (!methods) {
+            methods = 'all create get update delete exists count';
+        }
+
+        if (typeof methods === 'string') {
+            methods = methods.split(' ');
+        }
+
+        var name = resource.name;
+        var i;
+        var methodName;
+        if (name) { // if resource was defined
+            // remove extra slashes
+            name = trimSlashes(name);
+            // format url
+            var baseUrl = trimSlashes(resource.baseUrl || '');
+            var resourceName = trimSlashes(resource.url || '') || name;
+            var url = baseUrl + '/' + resourceName;
+            // loop through methods and set them up
+            for (i = 0; i < methods.length; i++) {
+                methodName = methods[i];
+                if ($methods.hasOwnProperty(methodName)) {
+                    if (resource.syntax === 'camel') {
+                        switch (methodName) {
+                            case 'all':     // getResources
+                                if (resource.methods && resource.methods.hasOwnProperty(methodName)) {
+                                    target[resource.methods[methodName].name] = $methods[methodName](url, options);
+                                } else {
+                                    target['get' + capitalize(name)] = $methods[methodName](url, options);
+                                }
+                                break;
+                            case 'create':  // createResource
+                            case 'update':  // updateResource
+                            case 'get':     // getResource
+                            case 'delete':  // deleteResource
+                                if (resource.methods && resource.methods.hasOwnProperty(methodName)) {
+                                    target[resource.methods[methodName].name] = $methods[methodName](url, options);
+                                } else {
+                                    target[methodName + singularizeCapitalize(name)] = $methods[methodName](url, options);
+                                }
+                                break;
+                            case 'count':   // getResourceCount
+                                if (resource.methods && resource.methods.hasOwnProperty(methodName)) {
+                                    target[resource.methods[methodName].name] = $methods[methodName](name);
+                                } else {
+                                    target['get' + singularizeCapitalize(name) + 'Count'] = $methods.get(url, options);
+                                }
+                                break;
+                            case 'exists':  // getResourceExists
+                                if (resource.methods && resource.methods.hasOwnProperty(methodName)) {
+                                    target[resource.methods[methodName].name] = $methods[methodName](name);
+                                } else {
+                                    target['get' + singularizeCapitalize(name) + 'Exists'] = $methods.get(url, options);
+                                }
+                                break;
+                            default:
+                                target[methodName + capitalize(name)] = $methods[methodName](url, options);
+                        }
+                    } else {
+                        target[name] = target[name] || {};
+                        target[name][methodName] = $methods[methodName](url, options);
+                    }
+                }
+            }
+        } else { // otherwise we place it on the global namespace
+            var methodOptions, path;
+            methods = resource.methods;
+            for (methodName in methods) {
+                if (methods.hasOwnProperty(methodName)) {
+                    methodOptions = methods[methodName];
+                    path = methodOptions.url || methodName;
+                    switch (methodOptions.type.toUpperCase()) {
+                        case 'POST':
+                            target[methodName] = $methods.create(path, options);
+                            break;
+                        case 'GET':
+                            target[methodName] = $methods.all(path, options);
+                            break;
+                        case 'PUT':
+                            target[methodName] = $methods.update(path, options);
+                            break;
+                        case 'DELETE':
+                            target[methodName] = $methods.delete(path, options);
+                            break;
+                    }
+                }
+            }
+        }
+    };
+});
---
+++ src/utils/ajax/resource.js
@@ -0,0 +1,141 @@
+/* global define */
+define('services.resource', ['isArray'], function (isArray) {
+    // https://gist.github.com/pduey/2764606
+
+    function clone(hash) {
+        return JSON.parse(JSON.stringify(hash));
+    }
+
+    function parseUrl(url, hash) {
+        for (var e in hash) {
+            if (hash.hasOwnProperty(e)) {
+                var regExp = new RegExp(':(' + e + ')\\b', 'g');
+                if (regExp.test(url)) {
+                    url = url.replace(regExp, hash[e]);
+                    delete hash[e];
+                }
+            }
+        }
+        return url;
+    }
+
+    function hashToSearch(hash) {
+        var search = '';
+        for (var k in hash) {
+            if (isArray(hash[k])) {
+                for (var i = 0; i < hash[k].length; i++) {
+                    search += !search ? '?' : '&';
+                    search += encodeURIComponent(k) + '=' + encodeURIComponent(hash[k][i]);
+                }
+            } else {
+                search += !search ? '?' : '&';
+                search += encodeURIComponent(k) + '=' + encodeURIComponent(hash[k]);
+            }
+        }
+        return search;
+    }
+
+    function Resource(name, id) {
+        this.$$id = id;
+        if (typeof name === 'string') {
+            this.$$name = name.replace(/^\/?(.*?)\/?$/, '$1'); // strip any '/' a the start or end of name
+            if(this.$$name[0] === '/') {
+                this.$$name = '/' + this.$$name;
+            }
+        }
+        this.$$parent = null;
+        this.$$params = null;
+    }
+
+    Resource.prototype.id = function (id) {
+        if (id === undefined) {
+            return this.$$id;
+        }
+        this.$$id = id;
+        return this;
+    };
+
+    Resource.prototype.name = function (name) {
+        if (name === undefined) {
+            return this.$$name;
+        }
+        this.$$name = name;
+        return this;
+    };
+
+    Resource.prototype.params = function (params) {
+        this.$$params = this.$$params || {};
+        if (arguments.length === 2) {
+            if (typeof arguments[1] === 'undefined') {
+                delete this.$$params[arguments[0]];
+            } else {
+                this.$$params[arguments[0]] = arguments[1];
+            }
+        } else if (typeof params === 'object') {
+            for (var e in params) {
+                if (params.hasOwnProperty(e)) {
+                    if (typeof params[e] === 'undefined') {
+                        delete this.$$params[e];
+                    } else {
+                        this.$$params = this.$$params || {};
+                        this.$$params[e] = params[e];
+                    }
+                }
+            }
+        }
+        return this;
+    };
+
+    Resource.prototype.resource = function (name, id) {
+        var resource = new Resource(name, id);
+        resource.$$parent = this;
+        return resource;
+    };
+
+    Resource.prototype.$$toUrl = function () {
+        var url = '';
+        if (this.$$parent) {
+            url += this.$$parent.$$toUrl();
+        }
+
+        if (typeof this.$$baseUrl === 'string') {
+            url = this.$$baseUrl;
+        } else {
+            if (this.$$name) {
+                if (this.$$name.match(/^(http(s?)|\/\/)/i)) { // if http(s)
+                    url = this.$$name;
+                } else {
+                    url += '/' + this.$$name;
+                }
+            }
+            if (this.$$id !== undefined) {
+                url += '/' + this.$$id;
+            }
+        }
+
+        return url;
+    };
+
+    Resource.prototype.toUrl = function () {
+        var url = this.$$toUrl();
+        if (this.$$params) {
+            var params = clone(this.$$params);
+            url = parseUrl(url, params);
+            url += hashToSearch(params);
+        }
+
+        return url;
+    };
+
+    return function (name, id) {
+        var resource;
+        if (typeof name === 'object') {
+            resource = new Resource(name.name, name.id);
+            resource.$$baseUrl = name.baseUrl;
+        } else {
+            resource = new Resource(name, id);
+        }
+        return resource;
+    };
+
+});
