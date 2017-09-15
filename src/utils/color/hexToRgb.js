6da4168fd51b48eb9b48aae0882971b6f22c4cea
wesjones <cybus10@gmail.com>
2017-09-15 13:15:54 -0600

    typo fix

--- src/utils/color/hexToRgb.js
+++ src/utils/color/hexToRgb.js
@@ -4,7 +4,7 @@ define('hexToRgb', function() {
         this.r = r && parseInt(r, 16) || 0;
         this.g = g && parseInt(g, 16) || 0;
         this.b = b && parseInt(b, 16) || 0;
-        this.a = a !== undefined ? parseFloat(a) || undefined;
+        this.a = a !== undefined ? parseFloat(a) : undefined;
         this.toString = function() {
             return 'rgb' + (this.a !== undefined ? 'a' : '') + '(' + this.r + ',' + this.g + ',' + this.b + (this.a !== undefined ? ',' + this.a : '') + ')';
         };
